254f1c6ca13df85d36ced59ec8fe0278b8bd6b28
Serghei Iakovlev <egrep@protonmail.ch>
2021-01-17 14:51:50 +0200

    Retry deploy and exit after 10 attempts

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,21 @@
 
 set -e
 
+interval=0
+retries=0
+
 while ! flask deploy; do
-  echo "Deploy command failed, retrying in 5 secs..."
-  sleep 5
+  interval=$((interval+3))
+  retries=$((retries+1))
+
+  if [ $retries -lt 10 ] ; then
+    echo "Deploy command failed, retrying in $interval secs..."
+    sleep $interval
+  else
+    >&2 echo "Deploy command failed after $retries attempts."
+    >&2 echo "Aborting."
+    exit 1
+  fi
 done
 
 show_help() {
