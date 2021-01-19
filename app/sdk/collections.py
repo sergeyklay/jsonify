82e3f82de472c3401661ad2c890e8d815113966e
Serghei Iakovlev <egrep@protonmail.ch>
2021-01-19 22:44:05 +0200

    Provide app.sdk.collections.merge()

--- app/sdk/collections.py
+++ app/sdk/collections.py
@@ -38,3 +38,21 @@ def path(data: dict, item, default=None):
             return default
 
     return reduce(getitem, item.split('.'), data)
+
+
+def merge(*objects):
+    """Merge one or more objects into a new object.
+
+    >>> merge({'a': 42}, {'foo': 'bar'})
+    {'a': 42, 'foo': 'bar'}
+    >>> merge({'a': 42}, {'foo': 'bar', 'a': 17})
+    {'a': 17, 'foo': 'bar'}
+    >>>  merge({'a': 17, 'foo': 'bar'})
+    {'a': 17, 'foo': 'bar'}
+    >>> merge({'a': 1}, {'b': 2}, {'c': 3}, {'a': 4})
+    {'a': 4, 'b': 2, 'c': 3}
+    """
+    result = {}
+    [result.update(obj) for obj in objects]
+
+    return result
