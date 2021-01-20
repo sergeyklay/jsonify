b14b29bb6fe6b1b9de279a75a46ef77b39f6a30c
Serghei Iakovlev <egrep@protonmail.ch>
2021-01-20 15:45:09 +0200

    Correct copyright notice

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 #
 # This file is part of the Jsonify.
 #
-# Copyright (c) 2021 airSlate Inc.
+# Copyright (c) 2021 airSlate, Inc.
 #
 # For the full copyright and license information, please view
 # the LICENSE file that was distributed with this source code.
