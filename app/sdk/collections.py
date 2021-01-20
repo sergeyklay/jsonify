6e35679ad26b1df14be792fe4bca7800308a7d1b
Serghei Iakovlev <egrep@protonmail.ch>
2021-01-20 15:46:50 +0200

    Install airslate client

--- app/sdk/collections.py
+++ app/sdk/collections.py
@@ -1,6 +1,6 @@
 # This file is part of the Jsonify.
 #
-# Copyright (c) 2021 airSlate Inc.
+# Copyright (c) 2021 airSlate, Inc.
 #
 # For the full copyright and license information, please view
 # the LICENSE file that was distributed with this source code.
@@ -38,21 +38,3 @@ def path(data: dict, item, default=None):
             return default
 
     return reduce(getitem, item.split('.'), data)
-
-
-def merge(*objects):
-    """Merge one or more objects into a new object.
-
-    >>> merge({'a': 42}, {'foo': 'bar'})
-    {'a': 42, 'foo': 'bar'}
-    >>> merge({'a': 42}, {'foo': 'bar', 'a': 17})
-    {'a': 17, 'foo': 'bar'}
-    >>>  merge({'a': 17, 'foo': 'bar'})
-    {'a': 17, 'foo': 'bar'}
-    >>> merge({'a': 1}, {'b': 2}, {'c': 3}, {'a': 4})
-    {'a': 4, 'b': 2, 'c': 3}
-    """
-    result = {}
-    [result.update(obj) for obj in objects]
-
-    return result
