b35e39a4ed52832ab4eb86a849e97f761d493f59
Serghei Iakovlev <egrep@protonmail.ch>
2021-02-04 00:21:18 +0200

    Clean no longer needed stuff

--- jsonify/api/organizations.py
+++ jsonify/api/organizations.py
@@ -24,7 +24,7 @@ def handle_connection():
     Gets notifications when the bot is added to a flow in an
     organization for the firs time.
     """
-    contents = request.get_json()
+    contents = request.get_json(silent=True)
 
     if not isinstance(contents, dict):
         raise BadRequest('No input data provided')
