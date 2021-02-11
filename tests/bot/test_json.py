cb80ae7928a6eae9da0b264ac5d7fa986bb87a39
Serghei Iakovlev <egrep@protonmail.ch>
2021-02-12 00:07:56 +0200

    Amend tests

--- tests/bot/test_json.py
+++ tests/bot/test_json.py
@@ -17,7 +17,7 @@ from bot.json import JsonDecoder
         ('[1, 2, 3, 4]', []),
         ('{"a": [1], "b": 42, "c": {"c1": 7, "c2": [1, 2]}}', ['a', 'c.c2']),
     ])
-def test_list_paths(provided, expected):
+def test_decoder_list_paths(provided, expected):
     """Create paths of all nested dictionary values."""
     decoder = JsonDecoder(provided)
 
@@ -26,3 +26,23 @@ def test_list_paths(provided, expected):
 
     assert actual == expected
 
+
+@pytest.mark.parametrize(
+    'provided,path,expected',
+    [
+        ('{}', '', []),
+        ('{}', 'a', []),
+        ('{}', 'a.b', []),
+        ('{"a": {"c": [1, 2]}, "b": [3, 4]}', 'a', ['c']),
+        ('{"a": {"c": [1, 2]}, "b": [3, 4]}', 'b', []),
+        ('{"a": {"c": [1, 2]}, "b": [3, 4]}', 'a.c', []),
+        ('{"a": {"c": {"k1": "v1", "k2": "v2"}}}', 'a.c', ['k1', 'k2']),
+        ('{"a": {"c": {"k1": "v1", "k2": "v2"}}}', 'x.y', []),
+    ])
+def test_decoder_fields(provided, path, expected):
+    decoder = JsonDecoder(provided)
+
+    actual = list(decoder.fields(path))
+    actual.sort()
+
+    assert actual == expected
